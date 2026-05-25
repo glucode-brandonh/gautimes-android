a3e60249678ee9b3d31430166e4d9ea9bc12b2e2
Brandon Hoffman <brandon.hoffman@glucode.com>
2026-05-25 10:44:36 +0200

    Initial Commit

---
+++ app/src/main/java/com/glucode/guatimes/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.glucode.guatimes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import com.glucode.guatimes.ui.theme.GuatimesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GuatimesTheme {
+                GuatimesApp()
+            }
+        }
+    }
+}
+
+@PreviewScreenSizes
+@Composable
+fun GuatimesApp() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Text(modifier = Modifier.padding(innerPadding), text = "Hello world")
+    }
+}
